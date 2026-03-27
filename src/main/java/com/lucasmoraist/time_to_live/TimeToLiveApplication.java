325351a405a0246bfb8c2ebee1da5083ad161579
lucasmoraist <luksmnt1101@gmail.com>
2026-03-26 22:57:56 -0300

    first commit

---
+++ src/main/java/com/lucasmoraist/time_to_live/TimeToLiveApplication.java
@@ -0,0 +1,13 @@
+package com.lucasmoraist.time_to_live;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeToLiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeToLiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasmoraist/time_to_live/TimeToLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasmoraist.time_to_live;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeToLiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
